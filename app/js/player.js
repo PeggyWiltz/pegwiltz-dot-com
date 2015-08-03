cda7e0d501a52fa20b99ca55ab19f2b8d8c65ed5
Peggy Wiltz <hengist2001@hotmail.com>
2015-08-03 12:09:09 -0700

    added node for testing, gulp build code, header and columns layout html and sass

--- app/js/player.js
+++ app/js/player.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
 
   $("#player__video").addClass('paused');
   $("#player__video").addClass('unmuted');
-
+  
   $('#player__button--play').click(function() {
     var player = $("#player__video");
     var playBtn = $('#player__button--play');
-
+    
     if (player.hasClass('paused')) {
       player.removeClass('paused');
       player[0].play();
@@ -19,32 +19,32 @@ $(document).ready(function() {
       playBtn.html('Play');
     }
   });
-
+  
    $('#player__button--mute').click(function() {
      var player = $("#player__video");
       var muteBtn = $('#player__button--mute');
-
+    
     if (player.hasClass('muted')) {
       player.removeClass('muted');
-      player.prop('muted', false);
+      player.prop('muted', false); 
       player.addClass('unmuted');
       muteBtn.html('Mute');
     } else {
       player.removeClass('unmuted');
-      player.prop('muted', true);
+      player.prop('muted', true); 
       player.addClass('muted');
       muteBtn.html('Unmute');
     }
   });
-
+  
 $('#player__video').on('timeupdate', function() {
     var video = $('#player__video');
-
-    var currentPos = video[0].currentTime;
-    var maxduration = video[0].duration;
-
-    var percentage = 100 * (currentPos  / maxduration);
+    
+    var currentPos = video[0].currentTime; 
+    var maxduration = video[0].duration; 
+    
+    var percentage = 100 * (currentPos  / maxduration); 
     $('#player__button--progress').css('width', percentage+'%');
 });
-
+  
 });
--- app/js/quote.js
+++ app/js/quote.js
@@ -87,3 +87,7 @@ window.onload = function () {
     }
 
 };
+
+
+
+
